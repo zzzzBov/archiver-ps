64b876faf5ce7a199327c54de88ca3d01bbec049
zzzzBov <zzzzBov@gmail.com>
2020-07-05 00:49:30 -0400

    Added help info to archiver

--- src/Archiver.psm1
+++ src/Archiver.psm1
@@ -1,4 +1,34 @@
+<#
+
+.SYNOPSIS
+
+Moves files from a source folder to an archive folder organized by year & month.
+
+.DESCRIPTION
+
+Moves files from a specified folder (defaults to downloads) to an archive
+folder with subfolders organized by years & months.
+
+.PARAMETER From
+
+Specifies the source folder to archive (defaults to "$home/Downloads").
+
+.PARAMETER To
+
+Specifies the destination folder (defaults to "$home/Archive") where the
+archive should be created.
+
+#>
+
 function Move-ToArchive {
+  param(
+    [string]
+    $From,
+
+    [string]
+    $To
+  )
+
   Write-Host "Hello, World!!!!"
 }
 
