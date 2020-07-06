989f5fc95f7311a9d253573e6c57210567c695f9
zzzzBov <zzzzBov@gmail.com>
2020-07-06 01:05:40 -0400

    Updated tests Created `New-TestFile` utility function Added simple test for archiving a single file

--- src/Archiver.Tests.ps1
+++ src/Archiver.Tests.ps1
@@ -9,9 +9,44 @@ InModuleScope $ThisModuleName {
       $result | Should -be $true;
     }
   
-    It "temporarily should return something else" {
-      $result = Move-ToArchive
-      $result | Should -be "$home\Downloads\"
+    It "Should move old files from the source folder to a subfolder in the archive" {
+      $dir = New-Guid
+      New-TestFile -Path "TestDrive:\$dir\Downloads\a.txt" -Created "2020-01-01" -Updated "2020-01-01"
+      
+      Move-ToArchive -From "TestDrive:\$dir\Downloads" -To "TestDrive:\$dir\Archive"
+
+      $archiveResult = Test-Path "TestDrive:\$dir\Archive\2020\01\a.txt"
+
+      $archiveResult | Should -be $true
+
+      $downloadsResult = Test-Path "TestDrive:\$dir\Downloads\a.txt"
+
+      $downloadsResult | Should -be $false
     }
   }
+}
+
+function global:New-TestFile {
+  param(
+    [string]
+    $Path,
+
+    [string]
+    $Created,
+
+    [string]
+    $Updated
+  )
+
+  $f = New-Item -Path $Path -ItemType File -Force
+  
+  if ($Created) {
+    $f.CreationTime = [DateTime]::Parse($Created)
+  }
+
+  if ($Updated) {
+    $f.LastWriteTime = [DateTime]::Parse($Updated)
+  }
+
+  return $f
 }
