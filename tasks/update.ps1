36e6dbf00276264cb31c47a91fa29c509506f49a
zzzzBov <zzzzBov@gmail.com>
2020-07-09 20:57:30 -0400

    updated update task

--- tasks/update.ps1
+++ tasks/update.ps1
@@ -4,6 +4,13 @@ Write over the existing module file, and then re-import the module.
 
 #>
 
-Copy-Item './Archiver/Archiver.psm1' -Destination "$home/Documents/WindowsPowerShell/Modules/Archiver/"
+$archiveModulePath = "$home/Documents/WindowsPowerShell/Modules/Archiver/"
+
+If (!(Test-Path -Path $archiveModulePath)) {
+  New-Item -Path $archiveModulePath -ItemType Directory -Force
+}
+
+Copy-Item './Archiver/Archiver.psm1' -Destination $archiveModulePath
+Copy-Item './Archiver/Archiver.psd1' -Destination $archiveModulePath
 
 Import-Module './Archiver/Archiver.psm1' -Force
